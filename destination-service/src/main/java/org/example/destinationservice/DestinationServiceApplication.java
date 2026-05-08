e94796a02fd4dcbc532096b93e673ab04c50a048
huangnv <huangnv@huangnvdeMacBook-Air.local>
2026-05-08 12:38:06 +0800

    chore(构建): 搭建 Maven 多模块工程

---
+++ destination-service/src/main/java/org/example/destinationservice/DestinationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.destinationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinationServiceApplication.class, args);
+    }
+
+}
---
+++ destination-service/src/test/java/org/example/destinationservice/DestinationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.destinationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
