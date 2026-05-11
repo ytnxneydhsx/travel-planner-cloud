17d42f33129b33e2dc6e9741d3b1e467885db371
huangnv <2453323231@qq.com>
2026-05-11 14:00:21 +0800

    refactor(discovery): route services through nacos

--- itinerary-service/src/main/java/org/example/itineraryservice/client/DestinationClient.java
+++ itinerary-service/src/main/java/org/example/itineraryservice/client/DestinationClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "destination-service", url = "${destination-service.base-url}")
+@FeignClient(name = "destination-service")
 public interface DestinationClient {
 
     @GetMapping("/destinations/{id}")
